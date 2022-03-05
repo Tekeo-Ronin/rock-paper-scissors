79d202ff0f1c7015cd7093bf9a2098e75328d9ec
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2022-03-05 16:02:54 +0100

    create random hands function

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-ds
+const userScore = 0;
+const compScore = 0;
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("comp-score");
+const rock = document.getElementById("r");
+const paper = document.getElementById("p");
+const scissors = document.getElementById("s");
+
+function getComputerChoise() {
+  const rand = 1 - 0.5 + Math.random() * (3 - 1 + 1);
+  if (Math.round(rand) == 1) {
+    return "r";
+  } else if (Math.round(rand) == 2) {
+    return "p";
+  } else {
+    return "s";
+  }
+}
+
+function game(userChoise) {
+  console.log(userChoise);
+}
+
+rock.addEventListener("click", () => {
+  game("r");
+});
+
+paper.addEventListener("click", () => {
+  game("p");
+});
+
+scissors.addEventListener("click", () => {
+  game("s");
+});
